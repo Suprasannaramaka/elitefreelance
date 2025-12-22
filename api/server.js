2e30e2a32a1407240d521f1ab832c8fc55fe9c45
Ramaka Suprasanna <ramakasuprasanna6@gmail.com>
2025-12-22 18:08:38 +0530

    init

--- api/server.js
+++ api/server.js
@@ -2,7 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 
 const app = express()
-
+try {
+  await mongoose.connect('mongodb+srv://suprasannaramaka:radhavittal@cluster0.j6qqlj6.mongodb.net/?appName=Cluster0');
+} catch (error) {
+  handleError(error);
+}
 app.listen(3000 , () => {
     console.log("Connected to backend!")
 })
