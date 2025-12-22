a72db07ee0251ad13b2496e390ca7374a3b03789
Ramaka Suprasanna <ramakasuprasanna6@gmail.com>
2025-12-22 18:13:47 +0530

    init

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express()
+dotenv.config();
 try {
   await mongoose.connect(process.env.MONGODB);
   console.log("Successfully connected to MongoDB");
