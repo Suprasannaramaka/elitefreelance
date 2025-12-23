e6fecb71775c5928437c476c8f4286efbfc28db0
Ramaka Suprasanna <ramakasuprasanna6@gmail.com>
2025-12-23 20:07:34 +0530

    init

---
+++ api/models/message.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const MessageSchema = new Schema({
+    conversationId:
+    {
+        type:String,
+        required:true,
+        unique: true,
+    },
+    userId:
+    {
+        type:String,
+        required:true,
+    },
+    desc:
+    {
+        type:String,
+        required:true,
+    },
+
+});
+export default mongoose.model('Message' , MessageSchema);
