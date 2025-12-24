3de083d45f46b1a75090e57c061ad093c6b57337
Ramaka Suprasanna <ramakasuprasanna6@gmail.com>
2025-12-24 23:32:47 +0530

    i

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,8 +1,7 @@
 import express from 'express';
 const router = express.Router();
-import {register, login , logout} from "../controllers/auth.controller.js";
+import {register} from "../controllers/auth.controller.js";
 
 router.post('/register',register)
-router.post('/login',login)
-router.post('/logout',logout)
+
 export default router;
