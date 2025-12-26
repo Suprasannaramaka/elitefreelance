a2a329aa8046a0dce760a1729704a6a07021c7ad
Ramaka Suprasanna <ramakasuprasanna6@gmail.com>
2025-12-26 16:04:33 +0530

    i

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import user from '../models/user.model.js';
+import {deleteUser} from '../controllers/user.controller.js';
 
 const router = express.Router();
- router.delete(':id' , deleteUser);
+ router.delete(':id', deleteUser);
 
  export default router;
